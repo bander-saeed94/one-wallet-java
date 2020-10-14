14b40a491f1b55afb5fe3cf99b62d0c8b2ffdd59
Bander Alshammari <bannadr1@hotmail.com>
2020-10-14 23:02:21 +0300

    first commit

---
+++ src/main/kotlin/com/onewallet/OneWallet/OneWalletApplication.kt
@@ -0,0 +1,11 @@
+package com.onewallet.OneWallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OneWalletApplication
+
+fun main(args: Array<String>) {
+	runApplication<OneWalletApplication>(*args)
+}
---
+++ src/test/kotlin/com/onewallet/OneWallet/OneWalletApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onewallet.OneWallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OneWalletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
